6b9e10eff577a00ac01d04d6efecfa0aa2497deb
Zayyn-khan <muhammadzaink.05@gmail.com>
2026-02-13 16:29:37 +0500

    Update Code

---
+++ Asp.net Core 2024-07D/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Asp.net_Core_2024_07D.Models;
+using Microsoft.EntityFrameworkCore;
+namespace Asp.net_Core_2024_07D.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // DbSets (Tables)
+        public DbSet<Voucher> Voucher { get; set; }
+    }
+}
